fe4296cae1280488b913c014c83b17336ac1a1d9
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-08-29 22:27:52 -0500

    feat: add PedroDriverControlled

---
+++ pedro/src/main/dev/nextftc/extensions/pedro/PedroDriverControlled.kt
@@ -0,0 +1,25 @@
+package dev.nextftc.extensions.pedro
+
+import dev.nextftc.hardware.driving.DriverControlledCommand
+import java.util.function.Supplier
+
+class PedroDriverControlled @JvmOverloads constructor(
+    drivePower: Supplier<Double>,
+    strafePower: Supplier<Double>,
+    turnPower: Supplier<Double>,
+    private val robotCentric: Boolean = true
+) : DriverControlledCommand(drivePower, strafePower, turnPower) {
+
+    override fun start() {
+        follower.startTeleopDrive()
+    }
+
+    override fun calculateAndSetPowers(powers: DoubleArray) {
+        val (drive, strafe, turn) = powers
+        follower.setTeleOpDrive(drive, strafe, turn, robotCentric)
+    }
+
+    override fun stop(interrupted: Boolean) {
+        if (interrupted) follower.breakFollowing()
+    }
+}
