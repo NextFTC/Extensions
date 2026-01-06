b2547677bbf81fc2f0b28bd76b334797576cbc5f
Zach Harel <zach@zharel.me>
2026-01-06 11:35:01 -0500

    feat: add driver-controlled command for Mecanum drivetrain (rr 1.1.0)
    Signed-off-by: Zach Harel <zach@zharel.me>

--- roadrunner/src/main/kotlin/dev/nextftc/extensions/roadrunner/NextFTCMecanumDrive.kt
+++ roadrunner/src/main/kotlin/dev/nextftc/extensions/roadrunner/NextFTCMecanumDrive.kt
@@ -5,6 +5,9 @@ import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.PoseVelocity2d
 import com.acmerobotics.roadrunner.PoseVelocity2dDual
 import com.acmerobotics.roadrunner.Time
+import com.acmerobotics.roadrunner.Vector2d
+import dev.nextftc.core.commands.utility.LambdaCommand
+import java.util.function.Supplier
 
 /**
  * Represents a RoadRunner Mecanum drivetrain from the QuickStart.
@@ -26,4 +29,31 @@ abstract class NextFTCMecanumDrive {
     abstract fun commandBuilder(beginPose: Pose2d): TrajectoryCommandBuilder
 
     open fun commandBuilder() = commandBuilder(getPose())
+
+    /**
+     * Creates a command for driver-controlled operation.
+     *
+     * @param xPower A function providing the x-axis power.
+     * @param yPower A function providing the y-axis power.
+     * @param headingPower A function providing the heading power.
+     * @param robotCentric Whether the controls are robot-centric (default: true).
+     * @return A [LambdaCommand] that updates the drivetrain powers.
+     */
+    @JvmOverloads fun driverControlledCommand(
+        xPower: Supplier<Double>,
+        yPower: Supplier<Double>,
+        headingPower: Supplier<Double>,
+        robotCentric: Boolean = true
+    ) = LambdaCommand()
+        .setUpdate {
+            var transVel = Vector2d(xPower.get(), yPower.get())
+
+            if (!robotCentric) {
+                transVel = getPose().heading * transVel
+            }
+
+            setDrivePowers(PoseVelocity2d(transVel, headingPower.get()))
+        }.setIsDone { false }
+        .requires(this)
+        .named("RoadRunner Driver Controlled")
 }
